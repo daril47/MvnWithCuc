fa98097d0bcb97c53cbe92c158a69e87de832f43
daril47 <krzysztof-gruza@wp.pl>
2017-09-13 14:41:01 +0200

    commit3

--- MvnWithCuc/src/test/java/stepdefinition/StepsFF.java
+++ MvnWithCuc/src/test/java/stepdefinition/StepsFF.java
@@ -6,45 +6,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-
 import datas.Datas;
 import junit.framework.Assert;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+public class StepsFF extends Datas {
 
-public class StepsFF  extends Datas {
+	WebDriver driver;
+	Datas datas = new Datas();
 
-	 WebDriver driver;
-	Datas datas=new Datas();
-	
 	@Given("^The http://automationpractice\\.com/index\\.php website is open$")
-	public void the_http_automationpractice_com_index_php_website_is_open() throws InterruptedException  {
-			datas.initializeFirefoxDriver(datas.driver);
-			Thread.sleep(5000);
-		   }
-	
+	public void the_http_automationpractice_com_index_php_website_is_open() throws InterruptedException {
+		datas.initializeFirefoxDriver(datas.driver);
+		Thread.sleep(5000);
+	}
+
 	@When("^User clicks on the Sign Up button$")
-	public void user_clicks_on_the_Sign_Up_button()  {
+	public void user_clicks_on_the_Sign_Up_button() {
 		datas.clickOnButton(datas.driver);
-		}
+	}
 
 	@When("^User provides wrong email and/or password$")
-	public void user_provides_wrong_email_and_or_password() throws InterruptedException  {
+	public void user_provides_wrong_email_and_or_password() throws InterruptedException {
 		datas.fillLoginDatas(datas.driver, "randomLogin", "randomPassword");
 		datas.clickOnSubmit(datas.driver);
-	    datas.singleSleep(datas.driver);
+		datas.singleSleep(datas.driver);
 
-	    
 	}
 
 	@Then("^Login will fail$")
-	public void login_will_fail()  {
-		
+	public void login_will_fail() {
+
 		System.out.println(datas.driver.findElement(By.xpath("//div[contains(@class,'alert')]//p")).isDisplayed());
-	   datas.driver.quit();
-	    
+		datas.driver.quit();
+
 	}
 
 }
